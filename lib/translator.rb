abaf77ec6d1fb129cca9ac02f10bb0619585327c
Nick <nick.moi@gmail.com>
2019-09-23 02:47:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,11 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   library = load_library(file_path)
-  library[:get_emoticon][emoticon]
+  if library[:get_emoticon][emoticon].nil?
+    "Sorry, that emoticon was not found"
+  else
+    library[:get_emoticon][emoticon]
+  end
 end
 
 def get_english_meaning
