ee74630ab50c58e0f1bd9e213327da6f60f43a53
Nick <nick.moi@gmail.com>
2019-09-23 02:14:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 def load_library
   # code goes here
   require "yaml"
-  emoticons = YAML.load_file('emoticons.yml')
+  emoticons = YAML.load_library('emoticons.yml')
 end
 
 def get_japanese_emoticon
